39279c90366175c34aeb6ba0a4233615971d88bd
WaqasMehmood100 <malikwaqas100@hotmail.com>
2021-07-07 13:49:18 +0500

    Commit code remove from tabor

--- PastaRecipe/tabBar/MyTabbarVC/MyTabbarVC.swift
+++ PastaRecipe/tabBar/MyTabbarVC/MyTabbarVC.swift
@@ -12,7 +12,7 @@ import UIKit
 import AZTabBar
 
 class MyTabbarVC: UIViewController {
-
+    
     var counter = 0
     var tabController:AZTabBarController!
     var audioId: SystemSoundID!
@@ -20,81 +20,12 @@ class MyTabbarVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //audioId = createAudio()
         // First Set Icons of tabs
         self.setIconsOnTabbar()
         //set delegate
         tabController.delegate = self
         //set child controllers
         self.initializaViewControllersOnTabbar()
-        
-
-
-        //customize
-
-//        let color = UIColor(red: 14.0/255, green: 122.0/255, blue: 254.0/255, alpha: 1.0)
-//
-//        tabController.selectedColor = color
-//
-//        tabController.highlightColor = color
-//
-//        tabController.highlightedBackgroundColor = #colorLiteral(red: 0.1803921569, green: 0.8, blue: 0.4431372549, alpha: 1)
-//
-        tabController.buttonsBackgroundColor = UIColor(named: "Theam 2")!
-        tabController.selectedColor = .white
-        //tabController.defaultColor = UIColor(named: "Theam 2")!
-//
-//        //tabController.highlightButton(atIndex: 2)
-//
-//        tabController.buttonsBackgroundColor = UIColor(red: (247.0/255), green: (247.0/255), blue: (247.0/255), alpha: 1.0)
-//
-//        tabController.selectionIndicatorHeight = 0
-//
-//        tabController.selectionIndicatorColor = color
-//
-//        tabController.tabBarHeight = 60
-//
-//        tabController.notificationBadgeAppearance.backgroundColor = .red
-//        tabController.notificationBadgeAppearance.textColor = .white
-//        tabController.notificationBadgeAppearance.borderColor = .clear
-//        tabController.notificationBadgeAppearance.borderWidth = 0.2
-//
-//        tabController.setBadgeText("!", atIndex: 4)
-//
-//        tabController.setIndex(10, animated: true)
-//
-////        tabController.setAction(atIndex: 3){
-////            self.counter = 0
-////            self.tabController.setBadgeText(nil, atIndex: 3)
-////        }
-////
-////        tabController.setAction(atIndex: 2) {
-////            self.tabController.onlyShowTextForSelectedButtons = !self.tabController.onlyShowTextForSelectedButtons
-////        }
-//
-////        tabController.setAction(atIndex: 4) {
-////            //self.tabController.setBar(hidden: true, animated: true)
-////        }
-//
-//        tabController.setIndex(1, animated: true)
-//
-//        tabController.animateTabChange = true
-//        tabController.onlyShowTextForSelectedButtons = false
-////        tabController.setTitle("Home", atIndex: 0)
-////        tabController.setTitle("Search", atIndex: 1)
-////        tabController.setTitle("Camera", atIndex: 2)
-////        tabController.setTitle("Feed", atIndex: 3)
-////        tabController.setTitle("Profile", atIndex: 4)
-//        tabController.font = UIFont(name: "AvenirNext-Regular", size: 12)
-//
-//        let container = tabController.buttonsContainer
-//        container?.layer.shadowOffset = CGSize(width: 0, height: -2)
-//        container?.layer.shadowRadius = 10
-//        container?.layer.shadowOpacity = 0.1
-//        container?.layer.shadowColor = UIColor.black.cgColor
-//
-//
-//        tabController.setButtonTintColor(color: #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1), atIndex: 0)
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -105,21 +36,9 @@ class MyTabbarVC: UIViewController {
         //set child controllers
         self.initializaViewControllersOnTabbar()
         
-
-
-        //customize
-
-//        let color = UIColor(red: 14.0/255, green: 122.0/255, blue: 254.0/255, alpha: 1.0)
-//
-//        tabController.selectedColor = color
-//
-//        tabController.highlightColor = color
-//
-//        tabController.highlightedBackgroundColor = #colorLiteral(red: 0.1803921569, green: 0.8, blue: 0.4431372549, alpha: 1)
-//
         tabController.buttonsBackgroundColor = UIColor(named: "Theam 2")!
         tabController.selectedColor = .white
-
+        
     }
     
     override var childForStatusBarStyle: UIViewController?{
@@ -142,7 +61,7 @@ class MyTabbarVC: UIViewController {
         sIcons.append(UIImage(named: "Place Marker")!)
         sIcons.append(UIImage(named: "Gift")!)
         sIcons.append(UIImage(named: "Headset")!)
-
+        
         tabController = .insert(into: self, withTabIcons: icons, andSelectedIcons: sIcons)
     }
     
@@ -161,8 +80,6 @@ class MyTabbarVC: UIViewController {
             tabController.setViewController(profileVC, atIndex: 1)
             
         }
-            
-        
         
         
         let CookWithChefVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "CookWithChefVC")
@@ -187,13 +104,6 @@ class MyTabbarVC: UIViewController {
         navigationController.navigationBar.barTintColor = #colorLiteral(red: 0.2039215686, green: 0.2862745098, blue: 0.368627451, alpha: 1)
         return navigationController
     }
-    
-//    func createAudio()->SystemSoundID{
-//        var soundID: SystemSoundID = 0
-//        let soundURL = CFBundleCopyResourceURL(CFBundleGetMainBundle(), "blop" as CFString?, "mp3" as CFString?, nil)
-//        AudioServicesCreateSystemSoundID(soundURL!, &soundID)
-//        return soundID
-//    }
     
     func actionLaunchCamera()
     {
@@ -262,34 +172,6 @@ extension MyTabbarVC: AZTabBarDelegate{
     }
     
 }
-//
-//extension MyTabbarVC: AZSearchViewDelegate{
-//
-//    func searchView(_ searchView: AZSearchViewController, didSearchForText text: String) {
-//        searchView.dismiss(animated: false, completion: nil)
-//    }
-//
-//    func searchView(_ searchView: AZSearchViewController, didTextChangeTo text: String, textLength: Int) {
-//        self.resultArray.removeAll()
-//        if textLength > 3 {
-//            for i in 0..<arc4random_uniform(10)+1 {self.resultArray.append("\(text) \(i+1)")}
-//        }
-//
-//        searchView.reloadData()
-//    }
-//
-//    func searchView(_ searchView: AZSearchViewController, didSelectResultAt index: Int, text: String) {
-//        searchView.dismiss(animated: true, completion: {
-//        })
-//    }
-//}
-//
-//extension MyTabbarVC: AZSearchViewDataSource{
-//
-//    func results() -> [String] {
-//        return self.resultArray
-//    }
-//}
 
 extension MyTabbarVC{
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
@@ -321,42 +203,3 @@ class LabelController: UIViewController {
     }
     
 }
-//
-//class ButtonController: UIViewController{
-//    class func controller(badgeCount:Int, currentIndex: Int )-> ButtonController{
-//        
-//        let controller = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ButtonController") as! ButtonController
-//        controller.badgeCount = badgeCount
-//        controller.currentIndex = currentIndex
-//        return controller
-//    }
-//    
-//    var badgeCount: Int = 0
-//    var currentIndex: Int = 0
-//    
-//    override func viewDidAppear(_ animated: Bool) {
-//        super.viewDidAppear(animated)
-//        
-//        for view in view.subviews{
-//            if view is UIButton{
-//                view.badge(text: nil)
-//            }
-//        }
-//        
-//        currentTabBar?.setBadgeText(nil, atIndex: currentIndex)
-//    }
-//    
-//    @IBAction func didClickButton(_ sender: UIButton) {
-//        badgeCount += 1
-//        
-//        //currentTabBar?.removeAction(atIndex: 2)
-//        
-//        if let tabBar = currentTabBar{
-//            tabBar.setBadgeText("\(badgeCount)", atIndex: currentIndex)
-//            sender.badge(text: "\(badgeCount)")
-//        }
-//    }
-//    
-//    
-//}
-
